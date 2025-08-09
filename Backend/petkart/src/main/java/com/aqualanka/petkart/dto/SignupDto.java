ef9b121cacdbacda67cafe970b0f05425cf30735
PasinduMadushanDias <pmdwijegunasinghe@gmail.com>
2025-08-09 16:11:10 +0530

    Setup backend project structure and add user signup & login APIs with MongoDB integration

---
+++ Backend/petkart/src/main/java/com/aqualanka/petkart/dto/SignupDto.java
@@ -0,0 +1,22 @@
+package com.aqualanka.petkart.dto;
+
+public class SignupDto {
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String phone;
+    public String dateOfBirth;
+    public String address;
+    public String city;
+    public String postalCode;
+    public boolean isSeller;
+    public boolean isBuyer;
+    public boolean isPetShop;
+    public boolean isBoth;
+    public String shopName;
+    public String shopLicense;
+    public String shopDescription;
+    public String yearsInBusiness;
+    public String specialties;
+    public String password;
+}
---
+++ Backend/petkart/src/test/java/com/aqualanka/petkart/PetkartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aqualanka.petkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
